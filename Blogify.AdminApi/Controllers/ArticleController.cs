36162af5a731e55f758d4b8d78ffb6917a920424
jackson <temp@example.com>
2025-06-15 20:33:36 +0800

    add 新增文章

--- Blogify.AdminApi/Controllers/ArticleController.cs
+++ Blogify.AdminApi/Controllers/ArticleController.cs
@@ -1,11 +1,25 @@
+using Blogify.AdminApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blogify.AdminApi.Controllers;
 
-
 public class ArticleController : Controller
 {
     public IActionResult Create()
+    {
+        var category = new List<Category>
+        {
+            new() { Id = 1, Name = "前端開發" },
+            new() { Id = 2, Name = "後端開發" },
+            new() { Id = 3, Name = "資料庫" },
+            new() { Id = 4, Name = "DevOps" },
+            new() { Id = 5, Name = "行動開發" }
+        };
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Article article)
     {
         return View();
     }
---
+++ Blogify.AdminApi/Models/Article.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blogify.AdminApi.Models;
+
+public class Article
+{
+    public int? Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    public string Excerpt { get; set; }
+
+    public string Author { get; set; }
+
+    [Required]
+    public int Category { get; set; }
+
+    public string Tags { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int ReadTime { get; set; } = 5;
+
+    [Url]
+    public string Image { get; set; }
+
+    public string Status { get; set; } = "draft";
+
+    public bool Featured { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+}
